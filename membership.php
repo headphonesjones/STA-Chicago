4b8ad1bb80cf7b7e50af55379c856017f25f1644
Michael Marais <michael@moondew.io>
2016-12-21 16:15:24 -0600

    Actually this adds those pages for membership and tabs

---
+++ membership.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include("components/header.php");?>
+    <title>The Society of Typographic Arts</title>
+</head>
+<body>
+<?php $title = "Membership"; include("components/nav.php");?>
+<div class="blackBackground">
+    <?php include("components/membership/tabs.php");?>
+</div>
+
+<div class="grayBackground">
+
+</div>
+
+<div class="blackBackground">
+
+</div>
+
+<div class="grayBackground">
+    <?php include("components/getInvolved.php");?>
+</div>
+
+<div class="blackBackground">
+    <?php include("components/sponsors.php");?>
+</div>
+<div class="blackBackground">
+    <?php include("components/footer.php");?>
+</div>
+<?php include("components/scripts.php");?>
+</body>
+</html>
