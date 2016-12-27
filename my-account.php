2f031dba454e587f1fa491736cf5016e714f03d6
Michael Marais <michael@moondew.io>
2016-12-27 12:01:54 -0600

    Add more html elements to my account page

--- my-account.php
+++ my-account.php
@@ -35,9 +35,118 @@
                                 <a class="button" href="#">Change Photo</a>
                             </div>
                         </div>
+                        <div class="row">
+                            <div class="twelve columns">
+                                <h3>MY PROFILE</h3>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="six columns">
+                                <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput">
+                            </div>
+                            <div class="six columns">
+                                <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="six columns">
+                                <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput">
+                            </div>
+                            <div class="six columns">
+                                <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="six columns">
+                                <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput">
+                            </div>
+                            <div class="six columns">
+                                <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="twelve columns">
+                                <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="three columns">
+                                <p>Available for work</p>
+                            </div>
+                            <div class="nine columns">
+                                <label class="switch">
+                                  <input type="checkbox">
+                                  <div class="slider round"></div>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="twelve columns">
+                                <h3>SOCIAL LINKS</h3>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="two columns">
+                                <label class="switch">
+                                    <input type="checkbox">
+                                    <div class="slider round"></div>
+                                </label>
+                            </div>
+                            <div class="ten columns">
+                                <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="two columns">
+                                <label class="switch">
+                                    <input type="checkbox">
+                                    <div class="slider round"></div>
+                                </label>
+                            </div>
+                            <div class="ten columns">
+                                <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="two columns">
+                                <label class="switch">
+                                    <input type="checkbox">
+                                    <div class="slider round"></div>
+                                </label>
+                            </div>
+                            <div class="ten columns">
+                                <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="twelve columns">
+                                <a class="button" href="#">Save</a>
+                            </div>
+                        </div>
                     </div>
                   </div>
-                  <div class="tab-pane" id="updatepassword">...</div>
+                  <div class="tab-pane" id="updatepassword">
+                    <div class="row">
+                        <div class="twelve columns">
+                            <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="twelve columns">
+                            <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="twelve columns">
+                            <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="twelve columns">
+                            <a class="button" href="#">Change</a>
+                        </div>
+                    </div>
+                  </div>
                   <div class="tab-pane" id="billinginformation">...</div>
                 </div>
             </div>
