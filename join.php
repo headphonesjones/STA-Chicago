45041c7913f950a34441305b957a83db7358bd5f
Michael Marais <michael@moondew.io>
2017-01-18 12:50:20 +0700

    Add new join page design

--- join.php
+++ join.php
@@ -5,79 +5,134 @@
     <title>The Society of Typographic Arts</title>
 </head>
 <body>
-<?php $title = "Join"; include("components/nav.php");?>
-<div class="blackBackground joinContainer">
+<?php $title = "Become a Member"; include("components/nav.php");?>
+<div class="grayBackground joinContainer">
     <div class="container">
         <div class="row">
-            <div class="twelve columns joinOption joinOptionStudent">
-                <h4>Student Member</h4>
-                <h5>$12.00</h5>
-                <p>Subscription period: 1 year No recurring payments</p>
+            <div class="three columns">
+                <h3 class="packageTitle whiteText">Student</h3>
+                <p class="price">$12/year</p>
+                <p>Here is a brief overview of who this membership is for and the primary benefits as to why they should join. Duis vitae uma eros. Proin nec dui.</p>
+                <a href="#" class="joinOption">Join Now</a>
+                <p class="feature">Up to 50% off event tickets for yourself</p>
+                <p class="feature">Discounted Competition Fees</p>
+                <p class="feature">Discounts from our Sponsors</p>
+                <p class="feature">Student-ONLY Events</p>
             </div>
-        </div>
-        <div class="row">
-            <div class="four columns joinOption">
-                <h4>Corporate</h4>
-                <h5>$250.00</h5>
-                <p>Subscription period: 1 year No recurring payments</p>
+            <div class="three columns">
+                <h3 class="packageTitle whiteText">Freelance</h3>
+                <p class="price">$10/month</p>
+                <p>Here is a brief overview of who this membership is for and the primary benefits as to why they should join. Duis vitae uma eros. Proin nec dui.</p>
+                <a href="#" class="joinOption">Join Now</a>
+                <p class="feature">Up to 30% off event tickets for yourself and 2 guests</p>
+                <p class="feature">STA Member Directory Access</p>
+                <p class="feature">Free Printed STA Publications</p>
+                <p class="feature">Discounts from our Sponsors</p>
+                <p class="feature">Automatic Renewal</p>
             </div>
-            <div class="four columns joinOption">
-                <h4>Freelance</h4>
-                <h5>$10.00</h5>
-                <p>Subscription period: Unlimited</p>
+            <div class="three columns">
+                <h3 class="packageTitle whiteText">Professional</h3>
+                <p class="price">$90/year</p>
+                <p>Here is a brief overview of who this membership is for and the primary benefits as to why they should join. Duis vitae uma eros. Proin nec dui.</p>
+                <a href="#" class="joinOption">Join Now</a>
+                <p class="feature">Up to 30% off event tickets for yourself and 2 guests</p>
+                <p class="feature">STA Member Directory Access</p>
+                <p class="feature">Free Printed STA Publications</p>
+                <p class="feature">Discounts from our Sponsors</p>
+                <p class="feature">Automatic Renewal</p>
+                <p class="feature">Discounted Competition Fees</p>
+                <p class="feature">Eligible to vote on STA issues</p>
+                <p class="feature">Eligible to Join our Board of Directors</p>
             </div>
-            <div class="four columns joinOption">
-                <h4>Member</h4>
-                <h5>$90.00</h5>
-                <p>Subscription period: 1 year No recurring payments</p>
+            <div class="three columns">
+                <h3 class="packageTitle whiteText">Corporate</h3>
+                <p class="price">$250/year</p>
+                <p>Here is a brief overview of who this membership is for and the primary benefits as to why they should join. Duis vitae uma eros. Proin nec dui.</p>
+                <a href="#" class="joinOption">Join Now</a>
+                <p class="feature">Up to 30% off event tickets for yourself and 2 guests</p>
+                <p class="feature">STA Member Directory Access</p>
+                <p class="feature">Free Printed STA Publications</p>
+                <p class="feature">Discounts from our Sponsors</p>
+                <p class="feature">Automatic Renewal</p>
+                <p class="feature">Discounted Competition Fees</p>
+                <p class="feature">Eligible to vote on STA issues</p>
+                <p class="feature">Eligible to Join our Board of Directors</p>
             </div>
         </div>
     </div>
-    <div class="container">
+</div>
+<div class="blackBackground faqContainer">
+    <div class="container joinFAQ">
         <div class="row">
-            <div class="twelve columns joinFAQ">
-                <h4><span class="whiteText">Are membership dues paid annually or one-time only?</span>
-
-                Professional and Corporate Memberships automatically renew annually, Freelance Memberships automatically renew monthly and Student Memberships have to be manually renewed every school year. You can cancel at any time. Student memberships must be renewed manually after the membership ends. Membership dues are nonrefundable.
-                <br>
-                    <span class="whiteText">Can I switch membership plans?</span>
-
-                To downgrade or upgrade your membership, you will need so after a membership has ended. For membership plans that automatically renew, you will need to cancel your current membership, then select a different plan when filling out the renewal form.
-                <br>
-                    <span class="whiteText">How do I cancel my membership?</span>
-
-                Canceling your membership is easy and convenient. You can do it directly within your account manager. No questions asked.
-                <br>
-                    <span class="whiteText">I cancelled my membership. Now what?</span>
-
-                You will continue to receive member benefits until the membership has gone full term. Canceling essentially tells our system that you would not like to be billed again for your current membership. Membership dues are nonrefundable.
-                <br>
-                    <span class="whiteText">My company wants to write a check for our Corporate Membership.</span>
-
-                Sure, no problem. We’re excited to have you join our family. Please contact our Membership Director and we'll get you set in no time.
-                <br>
-                    <span class="whiteText">Can I update my credit card to keep my membership up to date?</span>
-
-                You can update all of your billing information from the account menu in the top-right part of the screen after logging into you account.
-                <br>
-                    <span class="whiteText">Now that I’m a member. How can I get involved or help?</span>
-
-                We’re always looking for help in different areas. Please contact our Membership Director to see what's available.
-                <br>
-                    <span class="whiteText">I have an STA account, but I don't know my password.</span>
-
-                You can reset your password by following the instructions here.
-                </h4>
-
-
+            <div class="twelve columns">
+                <p class="smallTitle">Frequently Asked Questions</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="six columns">
+                <h4 class="whiteText">Are membership dues paid annually or one-time only?</h4>
+                <p>Professional and Corporate Memberships automatically renew annually, Freelance Memberships automatically renew monthly and Student Memberships have to be manually renewed every school year. You can cancel at any time. Student memberships must be renewed manually after the membership ends. Membership dues are nonrefundable.</p>
+            </div>
+            <div class="six columns">
+                <h4 class="whiteText">Can I switch membership plans?</h4>
+                <p>To downgrade or upgrade your membership, you will need so after a membership has ended. For membership plans that automatically renew, you will need to cancel your current membership, then select a different plan when filling out the renewal form.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="six columns">
+                <h4 class="whiteText">How do I cancel my membership?</h4>
+                <p>Canceling your membership is easy and convenient. You can do it directly within your account manager. No questions asked.</p>
+            </div>
+            <div class="six columns">
+                <h4 class="whiteText">I cancelled my membership. Now what?</h4>
+                <p>You will continue to receive member benefits until the membership has gone full term. Canceling essentially tells our system that you would not like to be billed again for your current membership. Membership dues are nonrefundable. </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="six columns">
+                <h4 class="whiteText">My company wants to write a check for our Corporate Membership.</h4>
+                <p>Sure, no problem. We’re excited to have you join our family. Please contact our Membership Director and we'll get you set in no time.</p>
+            </div>
+            <div class="six columns">
+                <h4 class="whiteText">Can I update my credit card to keep my membership up to date?</h4>
+                <p>You can update all of your billing information from the account menu in the top-right part of the screen after logging into you account. </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="six columns">
+                <h4 class="whiteText">Now that I’m a member. How can I get involved or help?</h4>
+                <p>We’re always looking for help in different areas. Please contact our Membership Director to see what's available.</p>
+            </div>
+            <div class="six columns">
+                <h4 class="whiteText">I have an STA account, but I don't know my password.</h4>
+                <p>You can reset your password by following the instructions here. </p>
             </div>
         </div>
     </div>
     <div class="bottomMargin"></div>
     <div class="plusDivider"></div>
 </div>
-
 <div class="grayBackground">
+    <div class="getInvolved textCenter">
+        <div class="container">
+            <div class="row">
+                <p class="smallTitle">Get Involved</p>
+                <a href="#">
+                    <h1>Come to an Event</h1>
+                </a>
+                <a href="#">
+                    <h1>Volunteer to help</h1>
+                </a>
+                <a href="#">
+                    <h1>Subscribe for updates</h1>
+                </a>
+            </div>
+        </div>
+        <div class="bottomMargin"></div>
+        <div class="plusDivider"></div>
+    </div>
+</div>
+<div class="blackBackground">
     <?php include("components/sponsors.php");?>
 </div>
 
